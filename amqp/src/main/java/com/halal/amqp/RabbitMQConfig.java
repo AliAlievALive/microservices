4f16a6893c2784fd4a1ce3aef97b9af924162f7e
AliAlievALive <madarat@mail.ru>
2022-01-21 15:50:54 +0300

    add rmq to another modules

--- amqp/src/main/java/com/halal/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/com/halal/amqp/RabbitMQConfig.java
@@ -17,7 +17,7 @@ public class RabbitMQConfig {
 
     @Bean
     public AmqpTemplate amqpTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jacksonConverter());
         return rabbitTemplate;
     }
