aa87107f90505a77c41db18fa3cec63b8603b938
AliAlievALive <madarat@mail.ru>
2022-01-25 11:46:54 +0300

    refactoring Microservices for working in k8s

--- notification/src/main/java/com/halal/notification/NotificationApplication.java
+++ notification/src/main/java/com/halal/notification/NotificationApplication.java
@@ -1,17 +1,18 @@
 package com.halal.notification;
 
-import com.halal.amqp.RabbitMQMessageProducer;
-import com.halal.notification.config.NotificationConfig;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {"com.halal.notification", "com.halal.amqp"}
 )
 @EnableEurekaClient
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
